e1cad06f4dec2d5d7df1ad40986f89b005d558ed
Andrew <frayz2507@gmail.com>
2023-09-25 19:32:23 +0300

    add login and register

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 function App() {
   return (
-    <div className="App">
-          <h1 className="text-3xl font-bold underline">
-            Hello world!
-          </h1>
+    <div>
+      <Login />
+      {/* <Register /> */}
     </div>
   );
 }
