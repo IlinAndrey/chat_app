314f2df57687775127c29f2abbb2016a79f58021
Laf <oleg.zipunov@mail.ru>
2023-10-01 01:13:56 +0300

    fixed login required to chat

--- django_react_chat_app/views.py
+++ django_react_chat_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
 
@@ -5,5 +6,8 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 def index(request):
     return render(request, 'index.html')
 
+
+@login_required
+@ensure_csrf_cookie
 def chat(request):
     return render(request, 'index.html')
