aee4634b453b2761aa8cdd192907e14f1abbd64f
Laf <oleg.zipunov@mail.ru>
2023-10-01 03:11:37 +0300

    redirect to chat if logged in from index

--- django_react_chat_app/views.py
+++ django_react_chat_app/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 @ensure_csrf_cookie
 def index(request):
+    if request.user.is_authenticated:
+        return redirect(to='chat')
     if request.method == 'POST':
         user = authenticate(username = request.POST.get('username', None), password = request.POST.get('password', None))
         if user:
