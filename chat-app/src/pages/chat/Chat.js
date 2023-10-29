1a9f7b43de7f4e063582ba6156c7e2387c4fe554
Andrey <frayz2507@gmail.com>
2023-10-29 03:12:28 +0300

    back to

--- chat-app/src/pages/chat/Chat.js
+++ chat-app/src/pages/chat/Chat.js
@@ -3,23 +3,17 @@ import SideBar from './SideBar'
 import InputBar from './InputBar'
 import useWebSocket, { ReadyState } from "react-use-websocket";
 import Cookies from 'js-cookie';
-import axios from 'axios';
 
 function Chat() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("")
   const [messages, setMessages] = useState([]);
   const [recipient, setRecipient] = useState(null);
   const [selectedUserId, setSelectedUserId] = useState(null);
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [isDialogSelected, setIsDialogSelected] = useState(false);
 
-  const [allMessages, setAllMessages] = useState([]);
-
-  const csrftoken = Cookies.get('csrftoken');
-  const sessionid = Cookies.get('sessionid');
-
+  const csrftoken = Cookies.get('csrftoken')
+  const sessionid = Cookies.get('sessionid')
+  // console.log(recipient)
+  
   const chatWebSocket = useWebSocket(`ws://127.0.0.1:8000/ws/chat/${selectedUserId}/`, {
     headers: {
       ['csrftoken']: csrftoken,
@@ -34,15 +28,7 @@ function Chat() {
     onMessage: (messageEvent) => {
       const receivedMessage = JSON.parse(messageEvent.data);
       console.log('Получено сообщение от сервера:', receivedMessage.message);
-  
-      if (Array.isArray(receivedMessage.message)) {
-        const newWebSocketMessages = receivedMessage.message.map(message => ({
-          text: message.text,
-        }));
-        setMessages(prevMessages => [...prevMessages, ...newWebSocketMessages]);
-      } else {
-        setMessages(prevMessages => [...prevMessages, { text: receivedMessage.message }]);
-      }
+      setMessages((prevMessages) => [...prevMessages, receivedMessage.message]);
     },
   });
   const { readyState, sendJsonMessage } = chatWebSocket;
@@ -62,61 +48,27 @@ function Chat() {
 
   const handleUserSelect = (userId) => {
     setSelectedUserId(userId);
-    setRecipient(userId);
-    setIsDialogSelected(true);
   };
 
-  useEffect(() => {
-    if (selectedUserId) {
-      axios.get(`/api/v1/directmessages/?recipient=${selectedUserId}`)
-        .then(response => {
-          const newMessages = response.data.map(message => ({
-            text: message.text,
-          }));
-          setAllMessages(prevMessages => [...prevMessages, ...newMessages]);
-          setLoading(false);
-        })
-        .catch(err => {
-          setError(err);
-          setLoading(false);
-        });
-    }
-  }, [selectedUserId]);
+
 
 
   return (
     <div>
-      <span>The WebSocket is currently {isDialogSelected ? connectionStatus : "Выберете диалог"}</span>
-      <div>{message}</div>
-      <SideBar onSelectUser={handleUserSelect} />
-      <InputBar sendJsonMessage={sendJsonMessage} setMessage={setMessage} handleSendMessage={handleSendMessage} />
+       <span>The WebSocket is currently {connectionStatus}</span>
+       <div>{message}</div>
+      <SideBar onSelectUser={handleUserSelect}/>
+      <InputBar sendJsonMessage={sendJsonMessage} setMessage={setMessage} handleSendMessage={handleSendMessage}/>
       <div>
         <h2>Сообщения</h2>
-        <div>
-          {loading ? (
-            <p>Загрузка...</p>
-          ) : error ? (
-            <p>Произошла ошибка: {error.message}</p>
-          ) : isDialogSelected ? (
-            <ul>
-              {Array.isArray(allMessages) ? (
-                allMessages.map((message, index) => (
-                  <li key={index}>{message.text}</li>
-                ))
-              ) : null}
-            </ul>
-          ) : null}
-        </div>
         <ul>
-          {Array.isArray(messages) ? (
-            messages.map((message, index) => (
-              <li key={index}>{message.text}</li>
-            ))
-          ) : null}
+          {messages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
         </ul>
       </div>
     </div>
-  );
+  )
 }
 
-export default Chat;
+export default Chat
